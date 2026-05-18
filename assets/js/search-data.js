a46053e61da05c0389a00fef29be2c4d092124b6
jeremyacollings <76605152+jeremyacollings@users.noreply.github.com>
2026-05-18 18:54:18 +0000

    Deploying to gh-pages from @ jeremyacollings/website2.0@3b918def5ac6d80b69277e413e3762a41553aac9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-competition-and-microbial-signatures",
+          title: 'Competition and Microbial Signatures',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/website2.0/projects/1_project/";
             },},{id: "projects-project-2",
